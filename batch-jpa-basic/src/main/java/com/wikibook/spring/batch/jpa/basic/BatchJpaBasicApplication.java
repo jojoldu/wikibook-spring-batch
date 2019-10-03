d7017c5be5961e4e43568984e3f2cb97be299af0
이동욱 <jojoldu@gmail.com>
2019-10-03 22:21:53 +0900

    Initial commit

---
+++ batch-jpa-basic/src/main/java/com/wikibook/spring/batch/jpa/basic/BatchJpaBasicApplication.java
@@ -0,0 +1,13 @@
+package com.wikibook.spring.batch.jpa.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchJpaBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchJpaBasicApplication.class, args);
+    }
+
+}
