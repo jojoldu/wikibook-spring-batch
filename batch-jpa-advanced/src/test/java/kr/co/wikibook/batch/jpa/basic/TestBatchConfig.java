5f92fb9bbbe08fa967a6ca3fd64c2c5e4cc00ceb
jojoldu <jojoldu@gmail.com>
2023-09-23 23:28:39 +0900

    jpa-advanced 예제 프로젝트 추가

---
+++ batch-jpa-advanced/src/test/java/kr/co/wikibook/batch/jpa/basic/TestBatchConfig.java
@@ -0,0 +1,10 @@
+package kr.co.wikibook.batch.jpa.basic;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableBatchProcessing
+public class TestBatchConfig {}
---
+++ batch-jpa-advanced/src/test/java/kr/co/wikibook/batch/jpa/basic/job/JpaPagingItemReaderJobConfigTest.java
@@ -0,0 +1,56 @@
+package kr.co.wikibook.batch.jpa.basic.job;
+
+import kr.co.wikibook.batch.jpa.basic.TestBatchConfig;
+import kr.co.wikibook.batch.jpa.basic.domain.teacher.Student;
+import kr.co.wikibook.batch.jpa.basic.domain.teacher.Teacher;
+import kr.co.wikibook.batch.jpa.basic.domain.teacher.TeacherRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBatchTest
+@SpringBootTest(classes = {JpaPagingItemReaderJobConfig.class, TestBatchConfig.class})
+class JpaPagingItemReaderJobConfigTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @BeforeEach
+    void setup() {
+        teacherRepository.deleteAll();
+    }
+
+    @Test
+    void test_jpa_paging() throws Exception {
+        //given
+        for(long i=1;i<=10;i++) {
+            String teacherName = i + "선생님";
+            Teacher teacher = new Teacher(teacherName, "수학");
+            teacher.addStudent(new Student(teacherName+"의 학생1"));
+            teacher.addStudent(new Student(teacherName+"의 학생2"));
+            teacherRepository.save(teacher);
+        }
+
+        JobParameters jobParameters = jobLauncherTestUtils.getUniqueJobParametersBuilder() // (2)
+                .toJobParameters();
+        //when
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob(jobParameters);
+
+        //then
+        assertThat(jobExecution.getStatus()).isEqualTo(BatchStatus.COMPLETED);
+    }
+}
