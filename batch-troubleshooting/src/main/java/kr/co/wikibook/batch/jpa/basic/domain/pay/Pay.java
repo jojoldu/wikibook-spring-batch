7101d0c540ae67e7f896a6f89d9e3545e39ee462
jojoldu <jojoldu@gmail.com>
2023-10-13 20:43:21 +0900

    트러블 슈팅 예제 추가

---
+++ batch-troubleshooting/src/main/java/kr/co/wikibook/batch/jpa/basic/domain/pay/Pay.java
@@ -0,0 +1,53 @@
+package kr.co.wikibook.batch.jpa.basic.domain.pay;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Pay {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long amount;
+    private String txName;
+    private LocalDateTime txDateTime;
+
+    public Pay() {
+    }
+
+    public Pay(Long amount, String txName, LocalDateTime txDateTime) {
+        this.amount = amount;
+        this.txName = txName;
+        this.txDateTime = txDateTime;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public String getTxName() {
+        return txName;
+    }
+
+    public LocalDateTime getTxDateTime() {
+        return txDateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Pay{" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", txName='" + txName + '\'' +
+                ", txDateTime=" + txDateTime +
+                '}';
+    }
+}
---
+++ batch-troubleshooting/src/main/java/kr/co/wikibook/batch/jpa/basic/job/writer/HibernateItemWriterJobConfig.java
@@ -0,0 +1,80 @@
+package kr.co.wikibook.batch.jpa.basic.job.writer;
+
+import kr.co.wikibook.batch.jpa.basic.domain.teacher.Student;
+import kr.co.wikibook.batch.jpa.basic.domain.teacher.Teacher;
+import org.hibernate.SessionFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.database.HibernateItemWriter;
+import org.springframework.batch.item.database.builder.HibernateItemWriterBuilder;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManagerFactory;
+import java.util.Arrays;
+
+@Configuration
+public class HibernateItemWriterJobConfig {
+    public static final String JOB_NAME = "hibernateItemWriterJob";
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+    private final SessionFactory sessionFactory;
+
+    public HibernateItemWriterJobConfig(JobBuilderFactory jobBuilderFactory, StepBuilderFactory stepBuilderFactory, EntityManagerFactory entityManagerFactory) {
+        this.jobBuilderFactory = jobBuilderFactory;
+        this.stepBuilderFactory = stepBuilderFactory;
+        this.sessionFactory = entityManagerFactory.unwrap(SessionFactory.class);
+    }
+
+    private int chunkSize;
+
+    @Value("${chunkSize:10}")
+    public void setChunkSize(int chunkSize) {
+        this.chunkSize = chunkSize;
+    }
+
+    @Bean(name = JOB_NAME)
+    public Job job() {
+        return jobBuilderFactory.get(JOB_NAME)
+                .start(step())
+                .build();
+    }
+
+    @Bean(name = JOB_NAME + "_step")
+    public Step step() {
+        return stepBuilderFactory.get(JOB_NAME + "_step")
+                .<Teacher, Teacher>chunk(chunkSize)
+                .reader(reader())
+                .processor(processor())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean(name = JOB_NAME + "_reader")
+    @StepScope
+    public ListItemReader<Teacher> reader() {
+        return new ListItemReader<>(Arrays.asList(
+                new Teacher("수학선생님", "수학"),
+                new Teacher("영어선생님", "영어")
+        ));
+    }
+
+    public ItemProcessor<Teacher, Teacher> processor() {
+        return teacher -> {
+            teacher.addStudent(new Student("신규 학생"));
+            return teacher;
+        };
+    }
+
+    public HibernateItemWriter<Teacher> writer() {
+        return new HibernateItemWriterBuilder<Teacher>()
+                .sessionFactory(sessionFactory)
+                .build();
+    }
+}
