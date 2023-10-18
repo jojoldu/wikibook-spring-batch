4beb0d5490e8a6a1b8d04f521810420d6157a3a7
jojoldu <jojoldu@gmail.com>
2023-10-19 08:34:56 +0900

    테스트 실패 수정

--- batch-scalling/src/test/java/kr/co/wikibook/batch/jpa/basic/job/multithread/MultiThreadStepJobConfigTest.java
+++ batch-scalling/src/test/java/kr/co/wikibook/batch/jpa/basic/job/multithread/MultiThreadStepJobConfigTest.java
@@ -14,6 +14,7 @@ import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
@@ -23,6 +24,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(SpringExtension.class)
 @SpringBatchTest
 @SpringBootTest(classes = {MultiThreadStepJobConfig.class, TestBatchConfig.class})
+@TestPropertySource(properties = {"job.name=" + MultiThreadStepJobConfig.JOB_NAME})
 class MultiThreadStepJobConfigTest {
 
     @Autowired
